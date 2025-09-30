1823ed2fec2296724b29c847ca9388db71007e72
Fhejs7g <adudaniel097@gmail.com>
2025-09-30 14:10:45 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+new WOW({
+    boxClass:     'wow',      // default
+    animateClass: 'animate__animated', // Animate.css prefix
+    offset:       0,          // distance from the bottom of the screen
+    mobile:       true,       // trigger on mobile
+    live:         true        // act on asynchronously loaded content
+  }).init();
+
+  
+
